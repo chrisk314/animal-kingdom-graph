c32d9e3ca87732438dab284263953f7782b9a738
Chris Knight <chrisk314@gmail.com>
2021-02-11 23:06:49 +0000

    WIP Extracts taxonomic hierarchy data from species pages

--- wiki-scraper/main.go
+++ wiki-scraper/main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/extensions"
 )
 
-const seedURL = "https://en.wikipedia.org/wiki/Animal"
+// const seedURL = "https://en.wikipedia.org/wiki/Animal"
+const seedURL = "https://en.wikipedia.org/wiki/Eunice_aphroditois"
 const allowedDomain = "en.wikipedia.org"
 const regexURLWikiNoFiles = "https://en.wikipedia.org/wiki/[^File:].+"
+
+type taxonomicLevel struct {
+	level string
+	value string
+}
+
+func createTaxonomicLevelFromSelection(s *goquery.Selection) taxonomicLevel {
+	taxLvlStrs := strings.Split(s.Text(), ":")
+	for i := range taxLvlStrs {
+		taxLvlStrs[i] = strings.TrimSpace(taxLvlStrs[i])
+	}
+	return taxonomicLevel{level: taxLvlStrs[0], value: taxLvlStrs[1]}
+}
 
 func main() {
 	c := colly.NewCollector(
@@ -33,10 +49,27 @@ func main() {
 		fmt.Println("visiting:", r.URL)
 	})
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// fmt.Println(link)
-		c.Visit(e.Request.AbsoluteURL(link))
+	c.OnHTML("#bodyContent", func(e *colly.HTMLElement) {
+		infoboxBiota := e.DOM.Find("table.infobox.biota")
+		if infoboxBiota.Length() != 1 {
+			return // No table.infobox.biota => this search path is a dead end.
+		}
+
+		taxLvlSel := infoboxBiota.Find("tr:contains('Kingdom')")
+		taxLvls := []taxonomicLevel{createTaxonomicLevelFromSelection(taxLvlSel)}
+		for !strings.Contains(taxLvlSel.Text(), "Species") {
+			taxLvlSel = taxLvlSel.Next()
+			taxLvls = append(taxLvls, createTaxonomicLevelFromSelection(taxLvlSel))
+		}
+
+		fmt.Println(taxLvls)
+
+		e.DOM.Find("a[href]").Each(func(_ int, s *goquery.Selection) {
+			link, exists := s.Attr("href")
+			if exists {
+				c.Visit(e.Request.AbsoluteURL(link))
+			}
+		})
 	})
 
 	// Start crawler at seed url.
